80425435658d8a8e26f7a94068e7d39ba21a4aff
lamphamit <phamtanlambe@gmail.com>
2023-10-11 17:35:53 +0700

    updated state for 03-steps

---
+++ 03-steps/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const messages = ["Learn react", "Apply for jobs", "Invest your new income"];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+
+      { isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3 </div>
+          </div>
+          <div className="message">
+            Step {step}: {messages[step - 1]}
+          </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+        )}
+    </>
+  );
+}
