768fc3578365a25fcc72d57baf91a8349a8e64bb
lamphamit <phamtanlambe@gmail.com>
2023-10-12 21:40:53 +0700

    upgrade 04-travel-list

---
+++ 04-travel-list/src/components/PackingList.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Item from "./Item";
+
+export function PackingList({ items, onDeleteItem, onToggleItems, onDeleteAllItems }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItems={onToggleItems} />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onDeleteAllItems}>Clear list</button>
+      </div>
+    </div>
+  );
+}
