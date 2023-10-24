3beccbaf2a62977fbb6e91b8c8a04669bee99d69
lamphamit <phamtanlambe@gmail.com>
2023-10-24 16:30:28 +0700

    fix: can't get data from fake api

---
+++ 10-react-quiz/src/Main.js
@@ -0,0 +1,4 @@
+function Main({ children }) {
+  return <main className="main">{children}</main>;
+}
+export default Main;
---
+++ 10-react-quiz/src/Question.js
@@ -0,0 +1,16 @@
+function Question({ question }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <div className="options">
+        {question.options.map((option) => (
+          <button className="btn btn-option" key={option}>
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Question;
